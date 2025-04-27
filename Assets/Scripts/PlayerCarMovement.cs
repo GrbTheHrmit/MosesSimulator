6d48886579cb63be821ab8af90e288747fa7828c
Grb The Hrmit <113816703+grobinsonbarr@users.noreply.github.com>
2025-04-27 15:29:28 -0600

    need to fix wheel forces next

--- Assets/Scripts/PlayerCarMovement.cs
+++ Assets/Scripts/PlayerCarMovement.cs
@@ -22,6 +22,9 @@ public class PlayerCarMovement : MonoBehaviour
     [SerializeField]
     private float MaxTurnAngle = 30f;
 
+    [SerializeField]
+    private float MaxSpringExtension = 1f;
+
     private static int WHEEL_FR = 0;
     private static int WHEEL_FL = 1;
     private static int WHEEL_BR = 2;
@@ -34,6 +37,8 @@ public class PlayerCarMovement : MonoBehaviour
     private Rigidbody rb;
     private Collider[] m_wheels;
     private float[] wheelSpringVelocity;
+
+    private float currentWheelSpeed = 0;
 
     private int StringToWheelIndex(string str)
     {
@@ -127,55 +132,67 @@ public class PlayerCarMovement : MonoBehaviour
 
         for (int i = m_wheels.Length - 1; i >= 0; i--)
         {
-            Vector3 newPosition = m_wheels[i].transform.localPosition;
-
-            if (Mathf.Abs(wheelSpringVelocity[i]) > 0.001f || Mathf.Abs(newPosition.y) > 0.001f)
+            // Spring Height
             {
-                float wheelHeight = m_wheels[i].transform.localPosition.y;
-                float springForce = -wheelHeight * SpringStrength; // Replace with variable later
-                float newWheelVelocity = (wheelSpringVelocity[i] * DamperStrength) + springForce * Time.fixedDeltaTime;
-                wheelSpringVelocity[i] = newWheelVelocity;
+                Vector3 newPosition = m_wheels[i].transform.localPosition;
 
-                newPosition += new Vector3(0, newWheelVelocity * Time.fixedDeltaTime, 0);
-            }
+                if (Mathf.Abs(wheelSpringVelocity[i]) > 0.001f || Mathf.Abs(newPosition.y) > 0.001f)
+                {
+                    float wheelHeight = m_wheels[i].transform.localPosition.y;
+                    float springForce = -wheelHeight * SpringStrength; // Replace with variable later
+                    float newWheelVelocity = (wheelSpringVelocity[i] * DamperStrength) + springForce * Time.fixedDeltaTime;
+                    wheelSpringVelocity[i] = newWheelVelocity;
 
-            if(i < 2)
-            {
-                m_wheels[i].transform.SetLocalPositionAndRotation(newPosition, Quaternion.Euler(0, lastMoveInput.x * MaxTurnAngle, 0));
-            }
-            else
-            {
-                m_wheels[i].transform.localPosition = newPosition;
-            }
-            
+                    newPosition += new Vector3(0, newWheelVelocity * Time.fixedDeltaTime, 0);
 
-            // Rotate input by car and wheel orientation
-            Vector3 moveInput = new Vector3(0, 0, lastMoveInput.y) * MoveSpeedIncrease;
+                    if (Mathf.Abs(newPosition.y) > MaxSpringExtension)
+                    {
+                        newPosition.y = Mathf.Clamp(newPosition.y, -MaxSpringExtension, MaxSpringExtension);
+                        wheelSpringVelocity[i] = 0;
+                    }
+                }
 
-            Vector3 pointVelocity = rb.GetPointVelocity(m_wheels[i].transform.position);
-            if (moveInput.sqrMagnitude  > 0)
-            {
                 if (i < 2)
                 {
-                    moveInput = gameObject.transform.rotation * Quaternion.Euler(0, lastMoveInput.x * MaxTurnAngle, 0) * moveInput;
-
+                    m_wheels[i].transform.SetLocalPositionAndRotation(newPosition, Quaternion.Euler(0, lastMoveInput.x * MaxTurnAngle, 0));
                 }
                 else
                 {
-                    moveInput *= 0.25f;
-                    moveInput = gameObject.transform.rotation * moveInput;
+                    m_wheels[i].transform.localPosition = newPosition;
                 }
-                moveInput.y = 0;
             }
-            
 
-            //Vector3 wheelVelocity = Vector3.MoveTowards(pointVelocity, pointVelocity + moveInput, MoveSpeedIncrease * Time.fixedDeltaTime);
+            // Movement Input
+            {
+                // Rotate input by car and wheel orientation
+                Vector3 moveInput = new Vector3(0, 0, lastMoveInput.y) * MoveSpeedIncrease;
+                //Vector3 moveInput = velocityChange;
 
-            //Vector3 velocityDiff = wheelVelocity - pointVelocity;
-            Vector3 XZOffset = m_wheels[i].transform.localPosition;
-            XZOffset.y = 0;
-            rb.AddForceAtPosition(moveInput, m_wheels[i].transform.position, ForceMode.Force);
-            Debug.DrawLine(m_wheels[i].transform.position, m_wheels[i].transform.position + moveInput, Color.red, 2);
+                //Vector3 pointVelocity = rb.GetPointVelocity(m_wheels[i].transform.position);
+                if (moveInput.sqrMagnitude > 0)
+                {
+                    if (i < 2)
+                    {
+                        moveInput = gameObject.transform.rotation * Quaternion.Euler(0, lastMoveInput.x * MaxTurnAngle, 0) * moveInput;
+
+                    }
+                    else
+                    {
+                        moveInput *= 0.25f;
+                        moveInput = gameObject.transform.rotation * moveInput;
+                    }
+                    moveInput.y = 0;
+                }
+
+
+                //Vector3 wheelVelocity = Vector3.MoveTowards(pointVelocity, pointVelocity + moveInput, MoveSpeedIncrease * Time.fixedDeltaTime);
+
+                //Vector3 velocityDiff = wheelVelocity - pointVelocity;
+                Vector3 XZOffset = m_wheels[i].transform.localPosition;
+                XZOffset.y = 0;
+                rb.AddForceAtPosition(moveInput, m_wheels[i].transform.position, ForceMode.Acceleration);
+                Debug.DrawLine(m_wheels[i].transform.position, m_wheels[i].transform.position + moveInput, Color.red, 2);
+            }
         }
         
 /*
@@ -192,7 +209,7 @@ public class PlayerCarMovement : MonoBehaviour
     {
         int idx = StringToWheelIndex(collision.contacts[0].thisCollider.name);
 
-        wheelSpringVelocity[idx] += collision.impulse.y * 0.2f;
+        wheelSpringVelocity[idx] += collision.impulse.y;
     }
 
 }
