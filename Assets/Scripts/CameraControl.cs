fe19632b492e69f6a98b90c0181fdcab69cf81fd
Grb The Hrmit <113816703+grobinsonbarr@users.noreply.github.com>
2025-05-19 20:46:33 -0600

    tweaking camera

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
 
 public class CameraControl : MonoBehaviour
 {
@@ -16,12 +17,24 @@ public class CameraControl : MonoBehaviour
     private float MinMoveSpeed = 5;
 
     [SerializeField]
-    private float MaxSwivelAngle = 90;
+    private float HSwivelSpeed = 90;
+    [SerializeField]
+    private float VSwivelSpeed = 15;
+
+    [SerializeField]
+    private float MaxHSwivelAngle = 120;
+    [SerializeField]
+    private float MaxVSwivelAngle = 45;
 
     public GameObject FocusObject;
     private PlayerCarMovement PlayerMovement;
 
+    private Vector2 lastInput;
     private Vector2 cameraInput;
+    private Vector3 lastRotationVector = Vector3.zero;
+    private GameObject CameraObject;
+
+    private float lastCameraPitch = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -46,22 +59,21 @@ public class CameraControl : MonoBehaviour
         {
             input.currentActionMap.FindAction("Camera").performed += OnCameraAction;
         }
+
+        CameraObject = GetComponentInChildren<Camera>().gameObject;
         
     }
 
     // Input Bindings
     public void OnCameraAction(InputAction.CallbackContext context)
     {
-        cameraInput = context.ReadValue<Vector2>();
+        lastInput = context.ReadValue<Vector2>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
         Vector3 playerVelocity = PlayerMovement.GetCurrentVelocity;
-
-        playerVelocity = Quaternion.Euler(-cameraInput.y * MaxSwivelAngle, cameraInput.x * MaxSwivelAngle, 0) * playerVelocity;
-
 
         float velocityFactor = (playerVelocity.magnitude - MinMoveSpeed) / PlayerMovement.GetMaxSpeed;
         float targetPlayerDist = velocityFactor * MaxDistToPlayer + (1 - velocityFactor) * MinDistToPlayer;
@@ -76,12 +88,12 @@ public class CameraControl : MonoBehaviour
 
             Vector3 lookDirection = playerVelocity;
             lookDirection.Normalize();
-            lookDirection.y = Mathf.Clamp(lookDirection.y, -0.2f, 0.2f);
+            lookDirection.y = Mathf.Clamp(lookDirection.y, -0.1f, 0.1f);
 
             if(lookDirection.magnitude <= 0.21f)
             {
                 lookDirection = FocusObject.transform.forward;
-                lookDirection.y = Mathf.Sign(playerVelocity.y) * 0.2f;
+                lookDirection.y = Mathf.Sign(playerVelocity.y) * 0.1f;
             }
 
             newPosition += Quaternion.FromToRotation(Vector3.forward, lookDirection.normalized) * new Vector3(0, CameraHeight, -targetPlayerDist);
@@ -94,6 +106,28 @@ public class CameraControl : MonoBehaviour
 
         transform.position = Vector3.MoveTowards(transform.position, newPosition, camSpeed * Time.fixedDeltaTime);
 
+        // Set Rotation, Using LookAt and then resetting pitch so we can set it in the camera object
+        Quaternion lastRotation = transform.rotation;
         transform.LookAt(FocusObject.transform);
+        float pitch = transform.eulerAngles.x; // Record pitch for later
+        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.y, 0); // Only rotate around Y
+        float interpValue = (HSwivelSpeed * Time.fixedDeltaTime) / Mathf.Max(Quaternion.Angle(transform.rotation, lastRotation), HSwivelSpeed * Time.fixedDeltaTime);
+        transform.rotation = Quaternion.Lerp(lastRotation, transform.rotation, interpValue); // Make sure we dont rotate further than time allows
+
+        // Set Camera Object location and rotation based on inputs and recorded pitch
+        cameraInput = Vector2.MoveTowards(cameraInput, lastInput, 3 * Mathf.Abs(lastInput.magnitude - cameraInput.magnitude) * Time.fixedDeltaTime);
+        Vector3 rotationVector = new Vector3(-cameraInput.y * MaxVSwivelAngle, cameraInput.x * MaxHSwivelAngle, 0);
+        Vector3 localOffset = Quaternion.Euler(0, -transform.eulerAngles.y, 0) * (PlayerMovement.transform.position - transform.position);
+        localOffset.y = 0;
+        Vector3 targetLocalPos = localOffset + Quaternion.Euler(rotationVector) * -localOffset;
+        CameraObject.transform.localPosition = targetLocalPos;
+
+        if(Mathf.Abs(pitch - lastCameraPitch) > VSwivelSpeed * Time.fixedDeltaTime)
+        {
+            pitch = lastCameraPitch + Mathf.Sign(pitch - lastCameraPitch) * VSwivelSpeed * Time.fixedDeltaTime;
+        }
+
+        CameraObject.transform.localRotation = Quaternion.Euler(rotationVector) * Quaternion.Euler(pitch, 0, 0);
+        lastCameraPitch = pitch;
     }
 }
